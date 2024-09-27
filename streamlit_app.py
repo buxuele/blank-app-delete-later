249ffc9e1ddf6d3a9a5685115917a262fbc58777
fanchuang <baogebuxuele@163.com>
2024-09-27 17:46:03 +0000

    test 01

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("大家好！")
+
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    " 测试一下部署过程。"
 )
